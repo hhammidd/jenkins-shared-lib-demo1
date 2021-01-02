3c799c4e41a8a521450cab681c9bfc0ee24cd737
hamid <hamid.shafikani@gmail.com>
2021-01-02 12:39:24 +0100

    initial-commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+pluralsight
+https://app.pluralsight.com/course-player?clipId=a2c7e5b7-d9ca-4def-bcb8-78b7b1414d14
--- vars/doEcho.groovy
+++ vars/doEcho.groovy
@@ -3,3 +3,7 @@ def call() {
         sh 'echo "Im from shared babe $BUILD_NUMBER the DEMO is $DEMO"'
     }
 }
+
+def goodBye(String name) {
+    println "GoodBye ${name}"
+}
